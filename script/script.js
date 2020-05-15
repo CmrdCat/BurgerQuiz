02cf6dc8ec654687d8d9147b81bbc3d4b94509ba
CmrdCat <63845158+CmrdCat@users.noreply.github.com>
2020-05-15 22:44:16 +0300

    day1 stream

--- script/script.js
+++ script/script.js
@@ -1,5 +1,4 @@
 'use strict';
-const test = 1; 
 
 document.addEventListener('DOMContentLoaded', function() {
    const btnOpenModal = document.querySelector('#btnOpenModal');
@@ -7,22 +6,64 @@ document.addEventListener('DOMContentLoaded', function() {
    const closeModal = document.querySelector('#closeModal');
    const questionTitle = document.querySelector('#question');
    const formAnswers = document.querySelector('#formAnswers');
+   const burgerMenu = document.querySelector('#burgerMenu');
+
+   burgerMenu.style.display = "none";
+
+   let clientWidth = document.documentElement.clientWidth; //Присваиваем в переменную ширину экрана при загрузке страницы
    
+   if (clientWidth < 768) {
+      burgerMenu.style.display = "flex";
+   } else {
+      burgerMenu.style.display = "none";
+   }
+
+   window.addEventListener('resize', function() {
+      clientWidth = document.documentElement.clientWidth;
+
+      if (clientWidth < 768) {
+         burgerMenu.style.display = "flex";
+      } else {
+         burgerMenu.style.display = "none";
+      }
+   }) //Событие отслеживает изменение размера окна
+
+   burgerMenu.addEventListener('click',() => {
+      burgerMenu.classList.add("active");
+      modalBlock.classList.add('d-block');
+      playTest();
+   });
+
+
+
+
    btnOpenModal.addEventListener('click',() => {
       modalBlock.classList.add('d-block');
       playTest();
    });
    closeModal.addEventListener('click', () => {
-      modalBlock.classList.remove('d-block')
+      modalBlock.classList.remove('d-block');
+      burgerMenu.classList.remove("active");
    });
 
+document.addEventListener('click',(event) => {
+   if (
+      !event.target.closest('.modal-dialog') && 
+      !event.target.closest('#btnOpenModal') &&
+      !event.target.closest('#burgerMenu')
+      ){
+      modalBlock.classList.remove('d-block');
+      burgerMenu.classList.remove("active");
+   }
+})
 
    const playTest = () => {
       const renderQuestions = () => {
          questionTitle.textContent = 'Какого цвета бургер вы хотите?';
          
          const burgerName = 'Стандарт';
-         const burgerImage = './image/burger.png'
+         const burgerImage = './image/burger.png';
+
          formAnswers.innerHTML = `<div class="answers-item d-flex flex-column">
                <input type="radio" id="answerItem1" name="answer" class="d-none">
                <label for="answerItem1" class="d-flex flex-column justify-content-between">
@@ -30,8 +71,7 @@ document.addEventListener('DOMContentLoaded', function() {
                   <span>${burgerName}</span>
                </label>
             </div>  
-            `
-         
+            `;
       };
       renderQuestions();
    }
